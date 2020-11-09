46ae23e495bdffde9ca4e34b29f776d189989244
Zack Amis <zachariah.amis@vizientinc.com>
2020-11-09 14:18:02 -0600

    silly example

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input() variablePassedToChild: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  parentVaraible: string = 'banana';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.updateVariable()
+    }, 1000)
+  }
+
+  updateVariable() {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < charactersLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return this.parentVaraible = result;
+  }
+
+}
